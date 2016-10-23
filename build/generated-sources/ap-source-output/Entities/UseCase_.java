c75e35737685ba63542e34f86a0077e20342a840
Mario Alberto Segura Oliva <marioalbertoseguraoliva@gmail.com>
2016-10-24 00:35:33 +0100

    Project Updated with some UI: Login Frame & User Frame

---
+++ build/generated-sources/ap-source-output/Entities/UseCase_.java
@@ -0,0 +1,16 @@
+package Entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-24T00:30:46")
+@StaticMetamodel(UseCase.class)
+public class UseCase_ { 
+
+    public static volatile SingularAttribute<UseCase, Integer> cost;
+    public static volatile SingularAttribute<UseCase, Integer> ucIdA;
+    public static volatile SingularAttribute<UseCase, String> ucName;
+    public static volatile SingularAttribute<UseCase, String> ucId;
+
+}
---
+++ build/generated-sources/ap-source-output/Entities/UserStory_.java
@@ -0,0 +1,18 @@
+package Entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-24T00:30:46")
+@StaticMetamodel(UserStory.class)
+public class UserStory_ { 
+
+    public static volatile SingularAttribute<UserStory, Integer> cost;
+    public static volatile SingularAttribute<UserStory, String> usName;
+    public static volatile SingularAttribute<UserStory, String> usBody;
+    public static volatile SingularAttribute<UserStory, String> deadline;
+    public static volatile SingularAttribute<UserStory, Integer> usIdA;
+    public static volatile SingularAttribute<UserStory, String> usId;
+
+}
